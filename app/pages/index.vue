8f26659e5e04cd9b05c954aabc3049cabb9486d2
iuiao7 <496224211@qq.com>
2024-08-30 09:26:06 +0000

    page structure

---
+++ app/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Index page</h1>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ app/pages/view-transition/demo1.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Demo1
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
