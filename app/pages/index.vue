993d52d1244365fc57a77631efa0133f9b3167a5
iuiao7 <496224211@qq.com>
2024-09-03 18:38:29 +0800

    show all routes

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,11 +1,18 @@
 <template>
-  <h1>Index page</h1>
+  <div>
+    <h1>Index page</h1>
+    <ul>
+      <li v-for="route of routes" :key="route.name">
+        <NuxtLink :to="route.path">{{ route.name }}</NuxtLink>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script lang="ts" setup>
+const router = useRouter();
 
+const routes = router.options.routes;
 </script>
 
-<style>
-
-</style>
+<style></style>
