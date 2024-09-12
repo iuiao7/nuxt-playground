5f0aea03258f8b8995dbc19ba0f652f92cf372de
iuiao7 <496224211@qq.com>
2024-09-12 15:20:03 +0800

    set compatibilityVersion to default

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,14 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
 
-  future: {
-    compatibilityVersion: 4,
-  },
-
   css: [
     "@unocss/reset/sanitize/sanitize.css",
     "@unocss/reset/sanitize/assets.css",
-    "~/assets/css/base-styles.css",
+    "assets/css/base-styles.css",
   ],
 
   experimental: {
