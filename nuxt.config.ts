a58746c1cebf952a57e8abaefcc801323e76bf67
iuiao7 <496224211@qq.com>
2024-09-14 15:20:29 +0800

    add shadcn-vue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,17 +3,13 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
 
-  css: [
-    "@unocss/reset/sanitize/sanitize.css",
-    "@unocss/reset/sanitize/assets.css",
-    "assets/css/base-styles.css",
-  ],
+  css: ["@unocss/reset/tailwind.css", "assets/css/base.css"],
 
   experimental: {
     viewTransition: true,
   },
 
-  modules: ["nuxt-typed-router", "@unocss/nuxt", "@nuxt/eslint"],
+  modules: ["nuxt-typed-router", "@unocss/nuxt", "@nuxt/eslint", "shadcn-nuxt"],
 
   devServer: {
     port: 5173,
--- uno.config.ts
+++ uno.config.ts
@@ -1,12 +1,24 @@
 // uno.config.ts
 import { defineConfig, presetAttributify, presetIcons, presetUno } from "unocss";
+import presetAnimations from "unocss-preset-animations";
+import { presetShadcn } from "unocss-preset-shadcn";
 
 export default defineConfig({
   // ...UnoCSS options
-  presets: [presetUno(), presetIcons(), presetAttributify()],
+  presets: [presetUno(), presetIcons(), presetAttributify(), presetAnimations(), presetShadcn()],
   theme: {
     fontFamily: {
       mono: "Operator Mono Lig, Operator Mono, JetBrains Mono, Monolisa, Monaco, Consolas, monospace",
+    },
+  },
+  content: {
+    pipeline: {
+      include: [
+        // the default
+        /\.(vue|svelte|[jt]sx|mdx?|astro|elm|php|phtml|html)($|\?)/,
+        // include js/ts files
+        "components/**/*.{js,ts}",
+      ],
     },
   },
 });
